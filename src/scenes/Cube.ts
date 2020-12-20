182038d78a56ec16d8ef266df587bed8c1e403a8
SAWADA Tadashi <cesare@mayverse.jp>
2020-12-21 00:30:28 +0900

    Refactor Cube

---
+++ src/scenes/Cube.ts
@@ -0,0 +1,70 @@
+import * as THREE from "three"
+
+export default class Cube {
+  width: number
+  height: number
+  scene: THREE.Scene
+  camera: THREE.Camera
+  renderer: THREE.Renderer
+  light: THREE.Light
+  box: THREE.Mesh
+
+  constructor(width: number, height: number) {
+    this.width = width
+    this.height = height
+
+    this.scene = new THREE.Scene()
+    this.camera = this.createCamera()
+    this.renderer = this.createRenderer()
+    this.light = this.createLight()
+    this.box = this.createBox()
+
+    this.scene.add(this.light)
+    this.scene.add(this.box)
+  }
+
+  private createCamera(): THREE.Camera {
+    const camera = new THREE.PerspectiveCamera(45, this.width / this.height, 1, 10000)
+    camera.position.set(0, 0, 1000)
+    return camera
+  }
+
+  private createRenderer(): THREE.Renderer {
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(this.width, this.height)
+    return renderer
+  }
+
+  private createLight(): THREE.Light {
+    const light = new THREE.DirectionalLight(0xffffff)
+    light.intensity = 2;
+    light.position.set(1, 1, 1)
+    return light
+  }
+
+  private createBox(): THREE.Mesh {
+    const geometry = new THREE.BoxGeometry(500, 500, 500)
+    const material = new THREE.MeshStandardMaterial({
+      color: 0x0000ff
+    })
+    const box = new THREE.Mesh(geometry, material)
+    return box
+  }
+
+  public getElement(): Element {
+    return this.renderer.domElement
+  }
+
+  public start() {
+    const update = () => {
+      requestAnimationFrame(update)
+
+      this.box.rotation.x += 0.01
+      this.box.rotation.y += 0.01
+
+      this.renderer.render(this.scene, this.camera)
+    }
+    update()
+  }
+}
--- src/views/RotatingCube.vue
+++ src/views/RotatingCube.vue
@@ -4,7 +4,7 @@
 
 <script lang="ts">
 import { Vue } from "vue-class-component"
-import * as THREE from "three"
+import Cube from "../scenes/Cube"
 
 export default class RotatingCube extends Vue {
   mounted() {
@@ -15,37 +15,9 @@ export default class RotatingCube extends Vue {
   }
 
   private setup(canvas: Element) {
-    const scene = new THREE.Scene()
-
-    const camera = new THREE.PerspectiveCamera(45, canvas.clientWidth / canvas.clientHeight, 1, 10000)
-    camera.position.set(0, 0, 1000)
-
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(canvas.clientWidth, canvas.clientHeight)
-    canvas.appendChild(renderer.domElement)
-
-    const geometry = new THREE.BoxGeometry(500, 500, 500)
-    const material = new THREE.MeshStandardMaterial({
-      color: 0x0000ff
-    })
-    const box = new THREE.Mesh(geometry, material)
-    scene.add(box)
-
-    const light = new THREE.DirectionalLight(0xffffff)
-    light.intensity = 2;
-    light.position.set(1, 1, 1)
-    scene.add(light)
-
-    const update = () => {
-      requestAnimationFrame(update)
-
-      box.rotation.x += 0.01
-      box.rotation.y += 0.01
-
-      renderer.render(scene, camera)
-    }
-    update()
+    const cube = new Cube(canvas.clientWidth, canvas.clientHeight)
+    canvas.appendChild(cube.getElement())
+    cube.start()
   }
 }
 </script>
